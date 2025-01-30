1aa22c0515fb66d6eeafeec283ee3430d3556363
torealdemir <aldemirtore@gmail.com>
2025-01-30 17:32:53 +0300

    fix:more changes on setting.py file

--- backend/backend_exercise_tracker/settings.py
+++ backend/backend_exercise_tracker/settings.py
@@ -9,10 +9,16 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
 
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("sSECRET_KEY", "default-secret-key")
+DEBUG = os.getenv("DEBUG", "True") == "True"
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
