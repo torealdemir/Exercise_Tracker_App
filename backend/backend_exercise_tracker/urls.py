8ddb336c5d69ae863eb59280a32d31cb6c70b23c
torealdemir <aldemirtore@gmail.com>
2025-01-30 17:06:33 +0300

    fix:still some changes on settings file and gitignore location changed

--- backend/backend_exercise_tracker/urls.py
+++ backend/backend_exercise_tracker/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for exercise_tracker_backend project.
+URL configuration for backend_exercise_tracker project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- backend/backend_exercise_tracker/wsgi.py
+++ backend/backend_exercise_tracker/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for exercise_tracker_backend project.
+WSGI config for backend_exercise_tracker project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exercise_tracker_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_exercise_tracker.settings')
 
 application = get_wsgi_application()
